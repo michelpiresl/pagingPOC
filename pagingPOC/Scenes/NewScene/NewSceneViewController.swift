83b404c7611945c44076ae5657069c4ffed5644b
Michel Pires Lourenço <michelpireslourenco@gmail.com>
2020-09-29 20:46:39 -0300

    messing xcodeproj

---
+++ pagingPOC/Scenes/NewScene/NewSceneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewSceneViewController.swift
+//  pagingPOC
+//
+//  Created by Michel Pires Lourenço on 29/09/20.
+//  Copyright © 2020 Michel Pires Lourenço. All rights reserved.
+//
+
+import UIKit
+
+class NewSceneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
